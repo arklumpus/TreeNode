arklumpus/TreeNode

commit 4601e52e164fd8568b4375ab6358ff80bc46d1b5

Allow spaces in node labels.

CSharp/TreeNode/NWKA.cs
@@ -443,7 +443,7 @@ namespace PhyloTree.Formats
                 {
                     if (attributeValue.Length > 0)
                     {
-                        string name = attributeName.ToString();
+                        string name = attributeName.ToString().Trim();
 
                         if (name.StartsWith("&", StringComparison.OrdinalIgnoreCase))
                         {
@@ -457,7 +457,7 @@ namespace PhyloTree.Formats
 
                         if (name.Equals("Name", StringComparison.OrdinalIgnoreCase))
                         {
-                            string value = attributeValue.ToString();
+                            string value = attributeValue.ToString().Trim();
 
                             if ((value.StartsWith("\"", StringComparison.OrdinalIgnoreCase) && value.EndsWith("\"", StringComparison.OrdinalIgnoreCase)) || (value.StartsWith("'", StringComparison.OrdinalIgnoreCase) && value.EndsWith("'", StringComparison.OrdinalIgnoreCase)))
                             {
@@ -469,16 +469,16 @@ namespace PhyloTree.Formats
                         else if (name.Equals("Support", StringComparison.OrdinalIgnoreCase))
                         {
                             supportCount = Math.Max(supportCount, 1);
-                            node.Support = double.Parse(attributeValue.ToString(), CultureInfo.InvariantCulture);
+                            node.Support = double.Parse(attributeValue.ToString().Trim(), CultureInfo.InvariantCulture);
                         }
                         else if (name.Equals("Length", StringComparison.OrdinalIgnoreCase))
                         {
                             lengthCount = Math.Max(lengthCount, 1);
-                            node.Length = double.Parse(attributeValue.ToString(), CultureInfo.InvariantCulture);
+                            node.Length = double.Parse(attributeValue.ToString().Trim(), CultureInfo.InvariantCulture);
                         }
                         else
                         {
-                            string value = attributeValue.ToString();
+                            string value = attributeValue.ToString().Trim();
                             if (double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out double result))
                             {
                                 node.Attributes.Add(name, result);
@@ -498,7 +498,7 @@ namespace PhyloTree.Formats
                         switch (lastSeparator)
                         {
                             case ':':
-                                if (double.TryParse(attributeName.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out double result))
+                                if (double.TryParse(attributeName.ToString().Trim(), NumberStyles.Any, CultureInfo.InvariantCulture, out double result))
                                 {
                                     if (lengthCount == 0)
                                     {
@@ -529,11 +529,11 @@ namespace PhyloTree.Formats
                                         name = newName;
                                     }
 
-                                    node.Attributes.Add(name, attributeName.ToString());
+                                    node.Attributes.Add(name, attributeName.ToString().Trim());
                                 }
                                 break;
                             case '/':
-                                if (double.TryParse(attributeName.ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out double result2))
+                                if (double.TryParse(attributeName.ToString().Trim(), NumberStyles.Any, CultureInfo.InvariantCulture, out double result2))
                                 {
                                     if (supportCount == 0)
                                     {
@@ -564,13 +564,13 @@ namespace PhyloTree.Formats
                                         name = newName;
                                     }
 
-                                    node.Attributes.Add(name, attributeName.ToString());
+                                    node.Attributes.Add(name, attributeName.ToString().Trim());
                                 }
                                 break;
                             case ',':
                                 bool isName = false;
 
-                                string value = attributeName.ToString();
+                                string value = attributeName.ToString().Trim();
 
                                 if ((value.StartsWith("\"", StringComparison.OrdinalIgnoreCase) && value.EndsWith("\"", StringComparison.OrdinalIgnoreCase)) || (value.StartsWith("'", StringComparison.OrdinalIgnoreCase) && value.EndsWith("'", StringComparison.OrdinalIgnoreCase)))
                                 {

CSharp/TreeNode/TreeNode.csproj
@@ -12,7 +12,7 @@
     <Product>TreeNode</Product>
     <PackageLicenseExpression>GPL-3.0-only</PackageLicenseExpression>
     <PackageProjectUrl>https://github.com/arklumpus/TreeNode</PackageProjectUrl>
-    <Version>1.5.2</Version>
+    <Version>1.5.3-a1</Version>
     <PackageIcon>icon.png</PackageIcon>
     <AllowUnsafeBlocks>True</AllowUnsafeBlocks>
   </PropertyGroup>

CSharp/TreeNode/Extensions.cs
@@ -303,11 +303,11 @@ namespace PhyloTree.Extensions
 
             if (count > 0)
             {
-                progressAction(1);
+                progressAction?.Invoke(1);
             }
             else
             {
-                progressAction(totalTrees);
+                progressAction?.Invoke(totalTrees);
             }
 
             if (finalSplits.Count > 0)
@@ -351,7 +351,7 @@ namespace PhyloTree.Extensions
                 escaped = false;
                 if (!openQuotes && !openApostrophe)
                 {
-                    while (Char.IsWhiteSpace(c))
+                    /*while (Char.IsWhiteSpace(c))
                     {
                         i = reader.Read();
 
@@ -363,7 +363,7 @@ namespace PhyloTree.Extensions
                         }
 
                         c = (char)i;
-                    }
+                    }*/
 
                     switch (c)
                     {
@@ -472,7 +472,7 @@ namespace PhyloTree.Extensions
         /// </summary>
         /// <param name="reader">The <see cref="TextReader"/> to read from.</param>
         /// <param name="eof">A <see cref="bool"/> indicating whether we have arrived at the end of the file.</param>
-        /// <param name="headingTrivia">A string containing any whitespace that was discarding before the start of the word.</param>
+        /// <param name="headingTrivia">A string containing any whitespace that was discarded before the start of the word.</param>
         /// <returns>The next word.</returns>
         public static string NextWord(this TextReader reader, out bool eof, out string headingTrivia)
         {

commit 928695ba66085a8c96296e2e64c1e787892e4bf1

Fixed regression introduced in 4601e52e164fd8568b4375ab6358ff80bc46d1b5 occurring with tree files with spaces between attribute names.

CSharp/TreeNode/NWKA.cs
@@ -441,7 +441,7 @@ namespace PhyloTree.Formats
                 }
                 else if ((eof || ((c2 == ':' || c2 == '/' || c2 == ',') && openSquareCount == 0 && openCurlyCount == 0)) && !escaped && !openQuotes && !openApostrophe)
                 {
-                    if (attributeValue.Length > 0)
+                    if (attributeValue.Length > 0 && !string.IsNullOrWhiteSpace(attributeValue.ToString()))
                     {
                         string name = attributeName.ToString().Trim();
 
@@ -493,7 +493,7 @@ namespace PhyloTree.Formats
                             }
                         }
                     }
-                    else if (attributeName.Length > 0)
+                    else if (attributeName.Length > 0 && !string.IsNullOrWhiteSpace(attributeName.ToString()))
                     {
                         switch (lastSeparator)
                         {

CSharp/TreeNode/TreeNode.csproj
@@ -12,7 +12,7 @@
     <Product>TreeNode</Product>
     <PackageLicenseExpression>GPL-3.0-only</PackageLicenseExpression>
     <PackageProjectUrl>https://github.com/arklumpus/TreeNode</PackageProjectUrl>
-    <Version>1.5.3</Version>
+    <Version>1.5.4</Version>
     <PackageIcon>icon.png</PackageIcon>
     <AllowUnsafeBlocks>True</AllowUnsafeBlocks>
   </PropertyGroup>